bbaea1bc903d0a56e09666cd13363ffd84daabba
dalloliogm <dalloliogm@gmail.com>
2009-05-21 18:14:28 +0200

    creating a graph with edges

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 debug_kgml2graph:
-	ipython -pylab -i -c "from parse_KGML import *; (graph, genes, reactions) = KGML2Graph('hsa00510_n-glycan.xml')"
+	ipython -pylab -i -c "from parse_KGML import *; (graph, nodes, genes, reactions) = KGML2Graph('hsa00510_n-glycan.xml')"
--- parse_KGML.py
+++ parse_KGML.py
@@ -22,10 +22,25 @@ def KGML2Graph(xmlfile):
     tree = ET.parse(xmlfile)
     for el in tree.getiterator('entry'):
         # get all genes or compounds, and associate ids to names
-        if el.attrib['type'] in ('gene', 'compound'):       # something else?
+        if el.attrib['type'] in ('gene', 'compound', 'map'):       # something else?
+            name = el.attrib['name']
+            id = el.attrib['id']
+#            if nodes.has_key(id):
+#                raise TypeError('over writing a key')
+            title = el.find('graphics').attrib['name']
+            nodes[id] = (name, title, el.attrib['type'])
+            if el.attrib['type'] == 'gene':
+                graph.add_node(title)
 
+    for rel in tree.getiterator('relation'):
+        e1 = rel.attrib['entry1']
+        e2 = rel.attrib['entry2']
+        print
+        print 'e1 ', nodes[e1]
+        print 'e2 ', nodes[e2] 
+        graph.add_edge(nodes[e1][1], nodes[e2][1])
    
-    return graph, genes, reactions
+    return graph, nodes, genes, reactions
 
 if __name__ == '__main__':
     KGML2Graph('hsa00510_n-glycan.xml')
