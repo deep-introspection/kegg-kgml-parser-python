92f341d6d93d742b533583b346ffb53730f17734
dalloliogm <dalloliogm@gmail.com>
2009-06-08 18:21:13 +0200

    added a Ngly test case

--- test/testIO.py
+++ test/testIO.py
@@ -5,16 +5,20 @@ class _BaseKGMLFile(U.TestCase):
     """
     base test class
     """
-    nodes = []
-    edges = []
+    known_values = {
+        'nodes': [],
+        'edges': []
+    }
+
     pathway_file = ''
     pathway_type = ''
 
     @classmethod
     def setUpClass(cls):
         logging.basicConfig(level=None)
-        if (not cls.edges or not cls.nodes or not cls.pathway_file):
+        if (not cls.known_values['edges'] or not cls.known_values['nodes'] or not cls.pathway_file):
             raise TypeError("incomplete test unit")
+
         (tree, graph, nodes, genes, reactions) = KGML2Graph(cls.pathway_file)
         cls.graph = graph
 
@@ -22,7 +26,10 @@ class _BaseKGMLFile(U.TestCase):
         pass
 
     def test_nodes(self):
-        self.assertEqual(self.nodes, self.graph.nodes())
+        """
+        Assert that Kegg2Graph parses nodes correctly
+        """
+        self.assertEqual(self.known_values['nodes'], self.graph.nodes())
 
 class _BaseKOFile(_BaseKGMLFile):
     """
@@ -39,18 +46,32 @@ class _BaseOrganismFile(_BaseKGMLFile):
     pathway_type = 'o'
 
 class test_GlycolisisHSA00010(_BaseOrganismFile):
-    """
-    """
     pathway_file = './data/hsa00010.xml'
-    nodes = ['PCK1', 'BPGM', 'ALDH3A1', 'GAPDH', 'LDHAL6A', 'TPI1', 'AKR1A1', 
-                'FBP1', 'PKLR', 'Carbon fixation in photosynthetic organisms', 
-                'PFKL', 'PGK1', 'PDHA1', 'GPI', 'Starch and sucrose metabolism', 
-                'DLD', 'ACSS2', 'GCK', 'PGM1', 'ENO1', 'DLAT', 'ALDOA', 'GALM', 
-                'Propanoate metabolism', 'G6PC', 'PGAM4', 'ALDH2', 
-                'Pentose phosphate pathway', 'ADH1A', 'Citrate cycle (TCA cycle)']
+    known_values = {
+        'nodes': [
+            'PCK1', 'BPGM', 'ALDH3A1', 'GAPDH', 'LDHAL6A', 'TPI1', 'AKR1A1', 
+            'FBP1', 'PKLR', 'Carbon fixation in photosynthetic organisms', 
+            'PFKL', 'PGK1', 'PDHA1', 'GPI', 'Starch and sucrose metabolism', 
+            'DLD', 'ACSS2', 'GCK', 'PGM1', 'ENO1', 'DLAT', 'ALDOA', 'GALM', 
+            'Propanoate metabolism', 'G6PC', 'PGAM4', 'ALDH2', 
+            'Pentose phosphate pathway', 'ADH1A', 'Citrate cycle (TCA cycle)'
+            ],
+        'edges': ['']
+    }
 
-    edges = ['']
-
-
-# nodes = ['ALG8', 'ALG9', 'GCS1', 'ST6GAL1', 'ALG2', 'ALG3', 'ALG1', 'ALG6', 'GANAB', 'ALG5', 'DOLPP1', 'ALG10B', 'DAD1', 'Other glycan degradation', 'Keratan sulfate biosynthesis', 'High-mannose type N-glycan biosynthesis', 'MGAT5B', 'B4GALT1', 'DPM3', 'DPM2', 'DPM1', 'DPAGT1', 'MAN2A1', 'MGAT1', 'MGAT2', 'MGAT3', 'Fructose and mannose metabolism', 'ALG11', 'ALG12', 'ALG13', 'ALG14', 'MAN1A2', 'Glycosylphosphatidylinositol(GPI)-anchor biosynthesis', 'MGAT4B', 'RFT1', 'FUT8']
+class test_NglycanHSA00510(_BaseOrganismFile):
+    pathway_file = './data/hsa00510.xml'
+    known_values = {
+        'nodes': [
+            'ALG8', 'ALG9', 'GCS1', 'ST6GAL1', 'ALG2', 'ALG3', 'ALG1', 'ALG6', 
+            'GANAB', 'ALG5', 'DOLPP1', 'ALG10B', 'DAD1', 'Other glycan degradation', 
+            'Keratan sulfate biosynthesis', 'High-mannose type N-glycan biosynthesis', 
+            'MGAT5B', 'B4GALT1', 'DPM3', 'DPM2', 'DPM1', 'DPAGT1', 'MAN2A1', 'MGAT1', 
+            'MGAT2', 'MGAT3', 'Fructose and mannose metabolism', 'ALG11', 'ALG12', 
+            'ALG13', 'ALG14', 'MAN1A2', 
+            'Glycosylphosphatidylinositol(GPI)-anchor biosynthesis', 
+            'MGAT4B', 'RFT1', 'FUT8'
+            ], 
+        'edges': ['']
+    }
 
