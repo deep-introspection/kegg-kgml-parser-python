d70726d1cf69bb5c13c1c79d4d9c8b1ef079e798
dalloliogm <dalloliogm@gmail.com>
2009-06-09 17:02:21 +0200

    redefined add_node method. KeggNode derived from str

--- KeggPathway.py
+++ KeggPathway.py
@@ -4,6 +4,7 @@ Basic class to represent Kegg pathways and nodes.
 
 You can use the function parse_KGML.KGML2Graph to create a KeggPathway object from a KGML file.
 """
+import networkx
 
 class KeggPathway(networkx.DiGraph):
     """
@@ -14,9 +15,19 @@ class KeggPathway(networkx.DiGraph):
                                 # convert all nodes in KeggNode objects
 
     """
-    pass
+    def add_node(self, n):
+        """
+        redefining add_node.
+        """
+        n = KeggNode(n)
 
-class KeggNode(object):
+        # TODO: call the original function instead of rewriting it
+        if n not in self.succ:
+            self.succ[n] = {}
+            self.pred[n] = {}
+
+
+class KeggNode(str):    # StrMixin? networkx.Node?
     """
     A node in a KeggPathway graph.
 
@@ -35,5 +46,14 @@ class KeggNode(object):
         """
         return self.name
 
+    def __repr__(self):
+        return self.name.__repr__()
 
+    def __eq__(self, other):
+        """
+        >>> gene = KeggNode('gene1')
+        >>> gene == 'gene1'
+        True
+        """
+        return self.name == other
 
